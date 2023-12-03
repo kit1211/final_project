457151a20d0be02c1985a933967e3d80ebd545ae
kit1211 <kittitat.yuti@bumail.net>
2023-12-03 08:46:32 +0700

    adaczxc

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,3 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'home::index',['filter' => 'authGuard']);
-$routes->group('/home', ['filter' => 'authGuard'], function($routes) {
-    $routes->get('customer', 'home::customer');
-    $routes->get('pos', 'home::pos');
-    $routes->get('product', 'home::product');
-    $routes->get('billing', 'home::billing');
-});
